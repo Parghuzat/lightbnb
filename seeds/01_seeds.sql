486bffc3ce7080b566f13ebf877010733b32306a
Parghuzat <paguzati@hotmail.com>
2021-11-25 00:10:40 -0800

    insert

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO users VALUES (1,'John', 'John@123.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users VALUES (2,'Kevin', 'Kevin@123.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users VALUES (3,'Aysha', 'Aysha@123.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties VALUES (1,1, 'GUanghua', 'Good','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',14,1,2,1,'canada','123 albert st','ottawa','ON','K1A 3A1','yes');
+INSERT INTO properties VALUES (2,2, 'Wansheng', 'Clean','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',14,1,2,1,'canada','123 albert st','ottawa','ON','K1A 3A1','yes');
+INSERT INTO properties VALUES (3,3, 'Hehe', 'Big','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',14,1,2,1,'canada','123 albert st','ottawa','ON','K1A 3A1','yes');
+
+INSERT INTO reservations VALUES (1,'2020-01-01','2021-01-01',1,1);
+INSERT INTO reservations VALUES (2,'2020-01-01','2021-01-01',2,2);
+INSERT INTO reservations VALUES (3,'2020-01-01','2021-01-01',3,3);
+
+INSERT INTO property_reviews VALUES (1,1,1,1,5,'good');
+INSERT INTO property_reviews VALUES (2,2,2,2,5,'good');
+INSERT INTO property_reviews VALUES (3,3,3,3,5,'good');
